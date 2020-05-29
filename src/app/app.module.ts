643a0969fd33a05f187611ecbafef356e6bcde88
Aaren Chabot <chabot.aaren@gmail.com>
2020-05-29 11:57:48 -0400

    hero-carousel shell, remove posts and mat components in cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,41 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-  MatInputModule,
-  MatCardModule,
-  MatButtonModule
-} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BusinessCardComponent } from './components/business-card/business-card.component';
 import { RecommendedBusinessesComponent } from './components/recommended-businesses/recommended-businesses.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HeroCarouselComponent } from './components/hero-carousel/hero-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
     BusinessCardComponent,
     RecommendedBusinessesComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HeroCarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
