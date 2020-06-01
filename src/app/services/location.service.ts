944be2a78174af19676e843b674dad3a5e1d93d4
Aaren Chabot <chabot.aaren@gmail.com>
2020-05-31 21:50:46 -0400

    location service init, set

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-  getVisitorCity = async () => {
-    const response = await (
-      await fetch('https://extreme-ip-lookup.com/json/')
-    ).json();
+  city: BehaviorSubject<string> = new BehaviorSubject('Toronto');
 
-    return response.city;
+  setCity = (city: string) => {
+    this.city.next(city);
+  };
+
+  initLocation = (): void => {
+    const storedCity = localStorage.getItem('city');
+    if (storedCity) {
+      this.city.next(storedCity);
+      console.log('Stored City: ', storedCity);
+      return;
+    }
+
+    fetch('https://extreme-ip-lookup.com/json/')
+      .then((response) => response.json())
+      .then((location) => this.city.next(location.city));
+    console.log('IP City: ', this.city.getValue());
+    localStorage.setItem('city', this.city.getValue());
   };
 }
