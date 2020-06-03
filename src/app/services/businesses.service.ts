366b313addc6272a4ce93ae0b827c807d695fa96
Aaren Chabot <chabot.aaren@gmail.com>
2020-06-02 21:04:33 -0400

    businesses wired to express app

--- src/app/services/businesses.service.ts
+++ src/app/services/businesses.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { IBusiness } from '@interfaces/business.model';
 import { BehaviorSubject, Observable } from 'rxjs';
 
@@ -6,69 +7,30 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class BusinessesService {
-  private barbers: BehaviorSubject<IBusiness[]> = new BehaviorSubject([
-    {
-      type: 'barber',
-      name: 'Barber 1',
-      address: '400 York Blvd, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/302/people',
-      isFeatured: true,
-      rating: 3.5,
-      link: ''
-    },
-    {
-      type: 'barber',
-      name: 'Hair Salon 2',
-      address: '140 King St, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/300/arch',
-      isFeatured: false,
-      rating: 4.5,
-      link: ''
-    },
-    {
-      type: 'barber',
-      name: 'Hair Salon 3',
-      address: '900 Front York Blvd, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/301/people',
-      isFeatured: false,
-      rating: 5.0,
-      link: ''
-    }
-  ]);
-  private salons: BehaviorSubject<IBusiness[]> = new BehaviorSubject([
-    {
-      type: 'salon',
-      name: 'Evolve Hair Studio',
-      address: '40 Fort York Blvd, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/301/arch',
-      isFeatured: true,
-      rating: 3.5,
-      link: ''
-    },
-    {
-      type: 'salon',
-      name: 'Hair Salon 2',
-      address: '140 Front York Blvd, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/300/people',
-      isFeatured: false,
-      rating: 4.5,
-      link: ''
-    },
-    {
-      type: 'salon',
-      name: 'Hair Salon 3',
-      address: '90 Fort York Blvd, Toronto, ON M5V 3Z3',
-      image: 'https://placeimg.com/550/300/arch',
-      isFeatured: false,
-      rating: 4.0,
-      link: ''
-    }
-  ]);
+  private featBarbers: BehaviorSubject<IBusiness[]> = new BehaviorSubject([]);
+  private featSalons: BehaviorSubject<IBusiness[]> = new BehaviorSubject([]);
+
+  constructor(private http: HttpClient) {}
 
   getFeaturedBarbers(): Observable<IBusiness[]> {
-    return this.barbers.asObservable();
+    this.http
+      .get<IBusiness[]>('http://localhost:3000/businesses')
+      .subscribe((response) => {
+        const filteredResponse = response.filter(
+          (biz) => biz.type === 'barber'
+        );
+        this.featBarbers.next(filteredResponse);
+      });
+    return this.featBarbers.asObservable();
   }
+
   getFeaturedSalons(): Observable<IBusiness[]> {
-    return this.salons.asObservable();
+    this.http
+      .get<IBusiness[]>('http://localhost:3000/businesses')
+      .subscribe((response) => {
+        const filteredResponse = response.filter((biz) => biz.type === 'salon');
+        this.featSalons.next(filteredResponse);
+      });
+    return this.featSalons.asObservable();
   }
 }
