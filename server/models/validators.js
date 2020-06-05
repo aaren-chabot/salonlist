1a2524da72fb905085dccc02bd3a8cd99610365b
Aaren Chabot <chabot.aaren@gmail.com>
2020-06-04 22:14:39 -0400

    mongoose model validators

--- server/models/validators.js
+++ server/models/validators.js
@@ -6,7 +6,22 @@ const phoneValidator = (v) => {
   return /\d{3}-\d{3}-\d{4}/.test(v);
 };
 
+const openCloseValidator = (v) => {
+  return v.open < v.close;
+};
+
+const postalZipValidator = (v) => {
+  if (v.country === 'Canada') {
+    return /^[A-Z]\d[A-Z][ -]?\d[A-Z]\d$/.test(v.zip);
+  }
+  if (v.country === 'USA') {
+    return /^[0-9]{5}(?:-[0-9]{4})?$/.test(v.zip);
+  }
+};
+
 module.exports = {
   imageUrlValidator,
-  phoneValidator
+  phoneValidator,
+  openCloseValidator,
+  postalZipValidator
 };
