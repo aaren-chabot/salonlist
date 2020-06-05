6c080c738a50cf02b2a58a13f4f30df8fcd1375f
Aaren Chabot <chabot.aaren@gmail.com>
2020-06-05 15:12:14 -0400

    mongoose blog model

--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,5 +1,47 @@
 const mongoose = require('mongoose');
+const { imageUrlValidator } = require('./validators');
 
-const blogSchema = new mongoose.Schema({});
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A blog title is required.'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  image: {
+    type: String,
+    validate: imageUrlValidator(v),
+    required: [true, 'A blog image is required.']
+  },
+  body: [
+    {
+      p: {
+        type: String,
+        trim: true
+      },
+      img: {
+        type: String,
+        trim: true,
+        validate: imageUrlValidator(v)
+      },
+      heading: {
+        type: String,
+        trim: true
+      },
+      required: [true, 'An article body is required.']
+    }
+  ],
+  author: {
+    type: mongoose.ObjectId,
+    ref: 'User'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = Blog = mongoose.model('blog', blogSchema, 'Blogs');
