9f72ef27e7c6cf9fc4973d5664e225985911d57d
Aaren Chabot <chabot.aaren@gmail.com>
2020-06-08 12:05:02 -0400

    imgHeight added to carousel comp

--- src/app/components/hero-carousel/hero-carousel.component.ts
+++ src/app/components/hero-carousel/hero-carousel.component.ts
@@ -7,15 +7,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeroCarouselComponent implements OnInit {
   @Input() search = true;
+  @Input() imgHeight = 500;
   config = {
     showNavigationArrows: false,
     showNavigationIndicators: false,
     interval: 4000
   };
-  images = [944, 1011, 984].map(
-    (n) => `https://picsum.photos/id/${n}/2000/500`
-  );
+  images: string[];
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.images = [944, 1011, 984].map(
+      (n) => `https://picsum.photos/id/${n}/2000/${this.imgHeight}`
+    );
+  }
 }
