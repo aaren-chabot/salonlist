e8cb45f9a769bcbc73a5d23adbbb223eb00b5ecf
Aaren Chabot <chabot.aaren@gmail.com>
2020-06-08 21:15:25 -0400

    grid and list view

--- src/app/pages/search/search-page.component.ts
+++ src/app/pages/search/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BusinessesService } from '@services/businesses.service';
+import { SearchService } from '@services/search/search.service';
+import { IBusiness } from '@interfaces/business.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-page',
@@ -8,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 export class SearchPageComponent implements OnInit {
   heading = 'Search Results';
   imgHeight = 300;
-  searchQuery: string;
+  searchResults: IBusiness[];
+  searchTerm: Observable<string>;
+  searchView: 'list' | 'grid' = 'list';
 
-  constructor() {}
+  constructor(
+    private businessesService: BusinessesService,
+    private searchService: SearchService
+  ) {}
 
-  ngOnInit() {}
+  toggleResultsView = (e, view) => {
+    this.searchView = view;
+  };
+
+  ngOnInit() {
+    this.businessesService.getFeaturedSalons().subscribe((results) => {
+      this.searchResults = results;
+    });
+    this.searchTerm = this.searchService.getSearchQuery();
+  }
 }
